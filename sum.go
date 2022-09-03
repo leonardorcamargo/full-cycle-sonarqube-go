8dfb7800a304ed46de0268320fe3f0152be5b71b
Leonardo Camargo <leonardorcamargo@gmail.com>
2022-09-03 20:38:33 -0300

    refactor: comment unnecessary functions

--- sum.go
+++ sum.go
@@ -10,14 +10,14 @@ func sum(a int, b int) int {
 	return a + b
 }
 
-func sub(a int, b int) int {
-	return a - b
-}
+// func sub(a int, b int) int {
+// 	return a - b
+// }
 
-func times(a int, b int) int {
-	return a * b
-}
+// func times(a int, b int) int {
+// 	return a * b
+// }
 
-func sumA(a int, b int) int {
-	return a + b + a
-}
+// func sumA(a int, b int) int {
+// 	return a + b + a
+// }
